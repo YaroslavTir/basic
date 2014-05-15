ede8da799dc0d3fd136b52aa323b1faaa41a1fc8
ymolodkov <yaroslavTir@gmail.com>
2014-05-15 19:40:56 +0400

    Route extension

--- src/main/groovy/org/openehealth/tutorial/SampleModelExtension.groovy
+++ src/main/groovy/org/openehealth/tutorial/SampleModelExtension.groovy
@@ -1,12 +1,20 @@
 package org.openehealth.tutorial
 
 import org.apache.camel.model.ProcessorType
-class SampleModelExtension {
+
+class SampleModelExtension {
 
      static extensions = {
          
          ProcessorType.metaClass.reverse = {
              delegate.transmogrify { it.reverse() }
+         }
+
+         ProcessorType.metaClass.setFileHeaderFrom = { String sourceHeader ->
+             delegate.setHeader('org.apache.camel.file.name') { exchange ->
+                 def destination = exchange.in.headers."$sourceHeader"
+                 destination ? "${destination}.txt" : 'default.txt'
+             }
          }
          
      }
--- src/main/groovy/org/openehealth/tutorial/SampleRouteBuilder.groovy
+++ src/main/groovy/org/openehealth/tutorial/SampleRouteBuilder.groovy
@@ -2,11 +2,20 @@ package org.openehealth.tutorial
 
 import org.apache.camel.spring.SpringRouteBuilder
 
+
 class SampleRouteBuilder extends SpringRouteBuilder {
 
     void configure() {
-        from('direct:input1').transmogrify { it * 2 }
+
+        from('jetty:http://0.0.0.0:8080/tutorial')     // receive client requests on http://0.0.0.0:8080/tutorial
+                .convertBodyTo(String.class)               // convert request input stream into a string
+                .to('direct:input1')                       // continue from direct:input1
+
+        from('direct:input1')
+                .transmogrify { it * 2 }                   // duplicate the request string
+                .setFileHeaderFrom('destination')          // set name of result file to be written (a custom DSL extension)
+                .to('file:target/output?append=false')     // replace content of file in target/output directory with body of in-message.
+
         from('direct:input2').reverse()
     }
-    
 }
